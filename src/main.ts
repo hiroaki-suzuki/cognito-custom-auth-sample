97476e5b05fcaf82325fdc7cf85787b78df4420e
Hiroaki Suzuki <rirazou05410@gmail.com>
2023-08-18 12:50:57 +0900

    Amplify UIの追加

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,20 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import {Amplify} from "aws-amplify";
 
 if (environment.production) {
   enableProdMode();
 }
+
+const conf = {
+  identityPoolId: '',
+  region: 'ap-northeast-1',
+  userPoolId: '',
+  userPoolWebClientId: '',
+}
+
+Amplify.configure(conf);
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
