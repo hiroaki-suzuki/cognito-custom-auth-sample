c81948c22a34e420b7a88b3a84a0a5a84894ce1a
Hiroaki Suzuki <rirazou05410@gmail.com>
2023-08-18 12:59:21 +0900

    HOME画面の追加

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  amplify: {
+    identityPoolId: '',
+    region: 'ap-northeast-1',
+    userPoolId: '',
+    userPoolWebClientId: '',
+    mandatorySignIn: true,
+  }
 };
 
 /*
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import {enableProdMode} from '@angular/core';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+import {AppModule} from './app/app.module';
+import {environment} from './environments/environment';
 import {Amplify} from "aws-amplify";
 
 if (environment.production) {
